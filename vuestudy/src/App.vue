59eabdfc4f2b4bdd61e38cd7898820b7c093c5c1
dnjswlekd <wj8014@gmail.com>
2023-07-21 02:03:58 +0900

    데이터바인딩

--- vuestudy/src/App.vue
+++ vuestudy/src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4>{{ nm1 }} 오피스텔</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ nm2 }} 오피스텔</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  //데이터바인딩을 위한 데이터 보관함
+  data(){
+    return {
+      nm1: "원지",
+      nm2: "세훈",
+      price1: 68.7,
+      price2: 80,
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
