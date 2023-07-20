309ffa978efc822eba36234df5ea845b6f9fed2f
dnjswlekd <wj8014@gmail.com>
2023-07-21 03:12:34 +0900

    v-for, databinding

--- vuestudy/src/App.vue
+++ vuestudy/src/App.vue
@@ -1,13 +1,22 @@
 <template>
+
+  <div class="menu">
+    <a v-for="navbar in menus" :key="navbar">{{ navbar }}</a>
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <h4>{{ nm1 }} 오피스텔</h4>
-    <p>{{ price1 }} 만원</p>
+  <div v-for="(consturction,i) in logo" :key="i">
+    <h1>{{ consturction }}</h1>
+    <h4 class="red" :style="style">{{ nm[i] }} 오피스텔</h4>
+    <p>{{ price[i] }} 만원</p>
+    <p>{{ products[i] }}</p>
+    <button @click="increase">허위매물신고</button><span> {{신고수[i]}} : {{ number }}</span>
   </div>
-  <div>
-    <h4>{{ nm2 }} 오피스텔</h4>
+  <!-- <div>
+    <h4 :style="style1">{{ nm2 }} 오피스텔</h4>
     <p>{{ price2 }} 만원</p>
-  </div>
+    <p>{{ products[2] }}</p>
+  </div> -->
 </template>
 
 <script>
@@ -17,15 +26,25 @@ export default {
   //데이터바인딩을 위한 데이터 보관함
   data(){
     return {
-      nm1: "원지",
-      nm2: "세훈",
-      price1: 68.7,
-      price2: 80,
+      신고수: ["신고수1", "신고수2"],
+      number: [0],
+      nm: ["원지", "세훈"],
+      price: ["68.7", "80"],
+      logo: ["원지네 부동산", "세훈이네 부동산"],
+      style: "color: green",
+      style1: "color: blue",
+      products: ["신도림", "범어", "개봉"],
+      menus: ["Home", "Shop", "Products", "About"]
     }
   },
   components: {
     
-  }
+  },
+  methods: {
+    increase(){
+      this.number;
+    }
+  },
 }
 </script>
 
@@ -36,6 +55,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  width: 20%;
+  margin: 0 auto;
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
