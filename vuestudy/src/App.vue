ab2a2c591901efcf8ee315f6a717d8e6d803e4a4
dnjswlekd <wj8014@gmail.com>
2023-07-21 09:26:07 +0900

    increase - 객체파일 생성해서 복습해보기

--- vuestudy/src/App.vue
+++ vuestudy/src/App.vue
@@ -4,13 +4,24 @@
     <a v-for="navbar in menus" :key="navbar">{{ navbar }}</a>
   </div>
 
+  <!-- modal -->
+  <div class="black-bg" v-if="openModal == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="openModal = 0">닫기</button>
+    </div>
+  </div>
+
+
+  <!-- 반복문 -->
   <img alt="Vue logo" src="./assets/logo.png">
   <div v-for="(consturction,i) in logo" :key="i">
     <h1>{{ consturction }}</h1>
     <h4 class="red" :style="style">{{ nm[i] }} 오피스텔</h4>
     <p>{{ price[i] }} 만원</p>
-    <p>{{ products[i] }}</p>
-    <button @click="increase">허위매물신고</button><span> {{신고수[i]}} : {{ number }}</span>
+    <p @click="openModal = true">{{ products[i] }}</p>
+    <button @click="increase">허위매물신고</button><span> 신고수 : {{ 신고수 }}</span>
   </div>
   <!-- <div>
     <h4 :style="style1">{{ nm2 }} 오피스텔</h4>
@@ -26,8 +37,8 @@ export default {
   //데이터바인딩을 위한 데이터 보관함
   data(){
     return {
-      신고수: ["신고수1", "신고수2"],
-      number: [0],
+      openModal: false,
+      신고수: 0,
       nm: ["원지", "세훈"],
       price: ["68.7", "80"],
       logo: ["원지네 부동산", "세훈이네 부동산"],
@@ -42,7 +53,7 @@ export default {
   },
   methods: {
     increase(){
-      this.number;
+      this.신고수++;
     }
   },
 }
@@ -67,5 +78,26 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 500px; height: 500px;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+  margin: 0 auto;
 }
 </style>
